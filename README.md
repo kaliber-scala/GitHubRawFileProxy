0ecdecba7880697d8d1a96044909ee33a0c2fcaf
ewestra <ewestra@rhinofly.nl>
2013-02-20 10:07:32 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,17 @@
+GitHub raw file proxy
+=====================
+
+This simple application can be used to proxy private raw files from GitHub.
+
+Configuration
+-------------
+
+``` scala
+	allowed.accessTokens=[3ab423aa747918asdd2a256b852c5a60580d3,3ab423aa7479186c96eb32asd5a60asd0d3]
+```
+
+Usage
+-----
+
+http://localhost:9000/accessToken/Owner/Repository/path/to/file.ext
+
---
+++ app/controllers/Application.scala
@@ -0,0 +1,79 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.ws.WS
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.JsValue
+import play.api.libs.iteratee.Enumerator
+import org.apache.commons.codec.binary.Base64
+import play.api.Play.current
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("GitHub Raw File Proxy"))
+  }
+
+  /*
+   {
+  	"sha": "dabc62e2fde3dd60547f17ff8a0010484f173928",
+  	"size": 301,
+  	"name": "file.ext",
+  	"path": "path/file.ext",
+  	"type": "file",
+  	"url": "https://api.github.com/repos/Rhinofly/repo/contents/path/file.ext",
+  	"git_url": "https://api.github.com/repos/Rhinofly/repo/git/blobs/dabc62e2fde3dd60547f17ff8a0010484f173928",
+  	"html_url": "https://github.com/Rhinofly/repo/blob/master/path/file.ext",
+  	"_links": {
+    	"self": "https://api.github.com/repos/Rhinofly/repo/contents/path/file.ext",
+    	"git": "https://api.github.com/repos/Rhinofly/repo/git/blobs/dabc62e2fde3dd60547f17ff8a0010484f173928",
+    	"html": "https://github.com/Rhinofly/repo/blob/master/path/file.ext"
+  	},
+  	"content": "base64encodedString",
+  	"encoding": "base64"
+	}
+   */
+  def getFileContents(json: JsValue): Array[Byte] = {
+    val content = (json \ "content").as[String]
+    
+    Base64 decodeBase64 content.getBytes
+  }
+
+  def proxy(accessToken: String, owner: String, repository: String, path: String) = Action {
+
+    val key = "allowed.accessTokens"
+
+    val app = implicitly[Application]
+    val allowedAccessTokens = app.configuration
+      .getStringList(key)
+      .getOrElse(throw new PlayException("Configuration error", "Could not find " + key + " in configuration"))
+
+    if (allowedAccessTokens contains accessToken) {
+
+      val url =
+        s"https://api.github.com/repos/$owner/$repository/contents/$path?access_token=$accessToken"
+
+      Async {
+        WS
+          .url(url)
+          .get
+          .map { response =>
+            val result = response.status match {
+              case 200 => Right(response.json)
+              case status => Left(s"Problem accessing github api, status '$status', $response.body")
+            }
+
+            result.fold(
+              problem => BadRequest(problem),
+              json =>
+                SimpleResult(
+                  header = ResponseHeader(200, Map(CONTENT_TYPE -> "application/octet-stream")),
+                  body = Enumerator(getFileContents(json))))
+          }
+      }
+
+    } else BadRequest(s"Access denied for access token '$accessToken'")
+  }
+
+}
